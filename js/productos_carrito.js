8d3cb0a424365a081e46be2a9aabd8e0b58d39d3
davidlongo <longodavidjulian@gmail.com>
2024-06-29 16:08:28 -0300

    los productos ahora se traen desde un archivo .JSON

--- js/productos_carrito.js
+++ js/productos_carrito.js
@@ -1,44 +1,10 @@
-class Producto {
-    constructor(identificador, nombre, precio, stock, rutaImg) {
-        this.identificador = parseInt(identificador);
-        this.nombre = nombre;
-        this.precio = parseFloat(precio);
-        this.stock = parseInt(stock);
-        this.rutaImg = rutaImg;
-    }
-}
-
-
 class Tienda {
     constructor() {
-        this.productos = [
-            new Producto(211, "Ernie Ball Hybrid Slinky", 55000, 10, "./css/assets/img/cuerdas/hybrid.png"),
-            new Producto(310, "Rotosound BS66", 70500, 5, "./css/assets/img/cuerdas/BS66.jpg"),
-            new Producto(410, "DR Neon Green", 76500, 3, "./css/assets/img/cuerdas/dr_green.jpg"),
-            new Producto(170, "D'addario EPBB170", 67500, 2, "./css/assets/img/cuerdas/daddarioEPBB170.jpg"),
-            new Producto(311, "Rotosound RS66LE", 70500, 7, "./css/assets/img/cuerdas/RS66LE.jpg"),
-            new Producto(212, "Ernie Ball Regular Slinky", 47000, 12, "./css/assets/img/cuerdas/regular.png"),
-            new Producto(213, "Ernie Ball Super Slinky", 43500, 3, "./css/assets/img/cuerdas/super.png"),
-            new Producto(160, "D'addario EXL160", 70500, 2, "./css/assets/img/cuerdas/daddarioEXL160.jpg"),
-            new Producto(411, "DR Neon Multi-color", 82850, 2, "./css/assets/img/cuerdas/dr_multi.jpg"),
-            new Producto(312, "Rotosound RS66LF", 43500, 3, "./css/assets/img/cuerdas/RS66LF.jpg"),
-            new Producto(214, "Ernie Ball Extra Slinky", 40500, 3, "./css/assets/img/cuerdas/extra.png"),
-            new Producto(313, "Rotosound RS66LH", 43500, 3, "./css/assets/img/cuerdas/RS66LH.jpg"),
-            new Producto(171, "D'addario EXL170", 43500, 3, "./css/assets/img/cuerdas/daddarioEXL170.jpg"),
-            new Producto(412, "DR Neon Orange", 80750, 4, "./css/assets/img/cuerdas/dr_orange.jpg"),
-            new Producto(215, "Ernie Ball Hyper Slinky", 40500, 3, "./css/assets/img/cuerdas/hyper.png"),
-            new Producto(314, "Rotosound RDB66LD", 43500, 5, "./css/assets/img/cuerdas/RDB66LD.jpg"),
-            new Producto(216, "Ernie Ball Power Slynky", 56000, 8, "./css/assets/img/cuerdas/power.png"),
-            new Producto(172, "D'addario EXL220-5", 43500, 3, "./css/assets/img/cuerdas/daddarioEXL220-5.jpg"),
-            new Producto(217, "Ernie Ball Beefy Slynky", 65500, 4, "./css/assets/img/cuerdas/beefy.png"),
-            new Producto(413, "DR Neon Red 5", 95650, 1, "./css/assets/img/cuerdas/dr_red.jpg"),
-            new Producto(315, "Rotosound RS66LN", 43500, 5, "./css/assets/img/cuerdas/RS66LN.jpg")
-
-        ];
+        this.productos = [];
     };
 }
 
-
+const tienda = new Tienda();
 
 const cardContainer = document.querySelector('.card-container');
 const carrito = document.getElementById('carrito');
@@ -47,30 +13,74 @@ const totalCarrito = document.getElementById('total-carrito');
 
 
 // Genera las cards para mostrar cada uno de los productos de la tienda
-function mostrarProductos(productos) {
+const mostrarProductos = (productos) => {
     cardContainer.innerHTML = '';
-
     productos.forEach(producto => {
         const card = document.createElement('div');
         card.className = 'card';
-
+        
         card.innerHTML = `
-            <img src="${producto.rutaImg}" alt="${producto.nombre}">
+            <img src="${producto.img}" alt="${producto.nombre}">
             <div class="card-content">
-                <h3>${producto.nombre}</h3>
-                <p>Stock: ${producto.stock}</p>
+                <h5>${producto.nombre}</h5>
+                <p>Stock: ${producto.stock < 5 ? 'Poco stock' : 'En stock'}</p>
                 <div class="price-buy">
                     <span class="price">$${(producto.precio)}</span>
-                    <button class="agregar-btn" data-id="${producto.identificador}">Agregar al carrito</button>
+                    <button class="agregar-btn" data-id="${producto.id}">Agregar al carrito</button>
                 </div>
             </div>
         `;
-
         cardContainer.appendChild(card);
     });
     aniadirALs();
 }
 
+// Carga los productos desde el JSON y luego los muestra
+const cargarProductos = async () => {
+    const resp = await fetch('./productos.json');
+    const productos = await resp.json();
+    tienda.productos = productos;
+    mostrarProductos(productos);
+}
+
+
+// Agrega eventos a todos los botones
+function aniadirALs() {
+    const btnAniadir = document.querySelectorAll('.agregar-btn');
+    btnAniadir.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const productoId = parseInt(btn.getAttribute('data-id'));
+            const productoAgregado = tienda.productos.find(producto => producto.id === productoId);
+
+            let itemEnLs = localStorage.getItem(`producto_${productoId}`);
+            itemEnLs = itemEnLs ? JSON.parse(itemEnLs) : { ...productoAgregado, cantidad: 0 };
+
+            if (itemEnLs.cantidad < productoAgregado.stock) {
+                itemEnLs.cantidad += 1;
+                localStorage.setItem(`producto_${productoId}`, JSON.stringify(itemEnLs));
+
+                Toastify({
+                    text: `${productoAgregado.nombre}\nse agregó al carrito`,
+                    className: 'info',
+                    offset: {
+                        x: 120,
+                        y: 35
+                    },
+                    style: {
+                        background: '#27ae60',
+                    }
+                }).showToast();
+
+                mostrarDesdeLs();
+            } else {
+                Swal.fire({
+                    imageUrl: `${productoAgregado.img}`,
+                    text: `${productoAgregado.nombre} no cuenta con suficiente stock...!!!`,
+                });
+            }
+        });
+    });
+}
 
 // Carga y muestra productos desde el LS en el carrito
 function mostrarDesdeLs() {
@@ -98,68 +108,29 @@ function mostrarDesdeLs() {
 };
 
 
-// Agrega eventos a todos los botones
-function aniadirALs() {
-    const btnAniadir = document.querySelectorAll('.agregar-btn');
-    btnAniadir.forEach(btn => {
-        btn.addEventListener('click', () => {
-            const productoId = parseInt(btn.getAttribute('data-id'));
-            const productoAgregado = tienda.productos.find(p => p.identificador === productoId);
-
-            let itemEnLs = localStorage.getItem(`producto_${productoId}`);
-            itemEnLs = itemEnLs ? JSON.parse(itemEnLs) : { ...productoAgregado, cantidad: 0 };
-
-            if (itemEnLs.cantidad < productoAgregado.stock) {
-                itemEnLs.cantidad += 1;
-                localStorage.setItem(`producto_${productoId}`, JSON.stringify(itemEnLs));
-
-                Toastify({
-                    text: `${productoAgregado.nombre}\nse agregó al carrito`,
-                    className: 'info',
-                    offset: {
-                        x: 120,
-                        y: 35
-                    },
-                    style: {
-                        background: '#27ae60',
-                    }
-                }).showToast();
-
-                mostrarDesdeLs();
-            } else {
-                Swal.fire({
-                    imageUrl: `${productoAgregado.rutaImg}`,
-                    text: `${productoAgregado.nombre} no cuenta con suficiente stock...!!!`,
-                });
-            }
-        });
-    });
-}
-
 // Filtra los productos por marca
-function productFilter() {
-    const productName = ["Ernie Ball", "D'addario", "Rotosound", "DR Neon"];
+function filtrarPorMarca() {
+    const marcasProductos = ["Ernie Ball", "D'addario", "Rotosound", "DR Neon"];
 
-    for (const name of productName) {
+    for (const marca of marcasProductos) {
         const ulNavLi = document.createElement('li');
-        ulNavLi.innerHTML = `${name}`;
+        ulNavLi.innerHTML = marca;
         ulNav.appendChild(ulNavLi);
 
         ulNavLi.addEventListener('click', () => {
-            const filteredProducts = tienda.productos.filter(producto => producto.nombre.includes(name));
+            const productosFiltrados = tienda.productos.filter(producto => producto.nombre.includes(marca));
             window.scrollTo({top: 0, behavior: 'smooth'});
-            mostrarProductos(filteredProducts);
-            
+            mostrarProductos(productosFiltrados);
         });
     }
 }
 
 
 //Muestra todos los productos al hacer click en el nombre de la pag
-shopName.addEventListener('click', () =>{
-    window.scrollTo({top: 0, behavior: 'smooth'});
+shopName.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
     mostrarProductos(tienda.productos)
-} );
+});
 
 //Elimina productos del carrito y LS
 carrito.addEventListener('click', (event) => {
@@ -174,10 +145,10 @@ carrito.addEventListener('click', (event) => {
 });
 
 
+// Muestra los productos después de que se cargue el DOM
+document.addEventListener('DOMContentLoaded', () => {
+    cargarProductos();
+    mostrarDesdeLs();
+    filtrarPorMarca();
+});
 
-// Se instacia la tienda  y luego se muestran los productos
-const tienda = new Tienda();
-mostrarProductos(tienda.productos);
-productFilter();
-
-document.addEventListener('DOMContentLoaded', mostrarDesdeLs);
